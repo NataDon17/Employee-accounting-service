d1d21228858a44b1ef1d60472ce299054722d8d6
https://github.com/NataDon17/Course2-Homework-11.git <don-nata@mail.ru>
2023-05-25 14:49:39 +0300

    первичный коммит для ДЗ spring boot

---
+++ src/main/java/ru/skypro/lessons/springboot/weblibraryhw/WebLibraryHwApplication.java
@@ -0,0 +1,13 @@
+package ru.skypro.lessons.springboot.weblibraryhw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebLibraryHwApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebLibraryHwApplication.class, args);
+    }
+
+}
