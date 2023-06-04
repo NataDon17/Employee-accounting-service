5701c91e4d1513ef76cb9fa47c6db93c6601d018
https://github.com/NataDon17/Course2-Homework-11.git <don-nata@mail.ru>
2023-06-04 14:33:59 +0300

    Выполнено ДЗ spring boot ч. 2

--- src/main/java/ru/skypro/lessons/springboot/weblibraryhw/repository/EmployeeRepository.java
+++ src/main/java/ru/skypro/lessons/springboot/weblibraryhw/repository/EmployeeRepository.java
@@ -2,8 +2,16 @@ package ru.skypro.lessons.springboot.weblibraryhw.repository;
 
 import ru.skypro.lessons.springboot.weblibraryhw.model.Employee;
 
-import java.util.List;
+import java.util.Collection;
 
 public interface EmployeeRepository {
-    public List<Employee> getAllEmployees();
+    Collection<Employee> getAllEmployees();
+
+    Employee createEmployee(Employee employee);
+
+    Employee updateEmployeeById(int id, Employee employee);
+
+    Employee getEmployeeById(int id);
+
+    void deleteEmployeeById(int id);
 }
