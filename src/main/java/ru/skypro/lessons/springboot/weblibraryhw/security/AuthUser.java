ba4b2e59bf03d73b0e41ce3be4aee854e7f0272d
https://github.com/NataDon17/Course2-Homework-11.git <don-nata@mail.ru>
2023-07-03 17:04:34 +0300

    Выполнено ДЗ spring liquibase поправила

--- src/main/java/ru/skypro/lessons/springboot/weblibraryhw/security/AuthUser.java
+++ src/main/java/ru/skypro/lessons/springboot/weblibraryhw/security/AuthUser.java
@@ -14,7 +14,7 @@ import lombok.Setter;
 @Setter
 public class AuthUser {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @Column(name = "username", nullable = false, unique = true)
     private String userName;
